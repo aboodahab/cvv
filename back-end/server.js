a0f680f6f80ada781c1ebf5d874d28e8e7bcc9be
Meow <abodkaramma77@gmail.com>
2024-08-02 17:23:00 +0300

    S

--- back-end/server.js
+++ back-end/server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const { jsPDF } = require("jspdf");
-const { createCanvas, loadImage } = require("canvas");
-const canvg = require("canvg");
+
 const app = express();
 
 app.use(express.json());
@@ -14,42 +13,34 @@ app.post("/test", function (req, res) {
 });
 
 app.post("/cvv", async function (req, res) {
-  try {
-    const svgPath = "call-outline.svg";
-    const svgContent = fs.readFileSync(svgPath, "utf8");
+  const doc = new jsPDF("p", "pc", "a5");
 
-    const canvas = createCanvas(200, 200);
-    const ctx = canvas.getContext("2d");
+  doc.setFontSize(22);
 
-    // Use canvg with the canvas context and SVG content
-    canvg(canvas, svgContent, {
-      ignoreMouse: true,
-      ignoreAnimation: true,
-      ignoreDimensions: true,
-    });
-
-    const pngBuffer = canvas.toBuffer("image/png");
-    const pngBase64 = pngBuffer.toString("base64");
-    const imageFormat = "PNG";
-
-    const doc = new jsPDF("p", "mm", "a4");
-
-    doc.addImage(pngBase64, imageFormat, 10, 10, 100, 100);
-    doc.setFontSize(22);
-    doc.text(`${req.body.names}`, 19, 14);
+  if (req.body.names.length < req.body.work.length) {
+    console.log("ha");
+    doc.text(`${req.body.names}`, 17, 14);
     doc.setFontSize(14);
-    doc.text(`${req.body.work}`, 19.5, 16);
-    doc.setFontSize(12);
-    doc.text(`${req.body.num}`, 10.5, 19);
+    doc.text(`${req.body.work}`, 16.5, 16.5);
+    doc.setFontSize(11);
+    doc.text(`${req.body.num}`, 9.5, 19);
 
     doc.save(`ess.pdf`);
 
-    console.log(req.body, "arwarwr");
-    res.json({ jse: "sekjrlwjr" });
-  } catch (error) {
-    console.error("Error generating PDF:", error);
-    res.status(500).json({ error: "Failed to generate PDF" });
+    return;
   }
+  console.log("ho");
+  doc.text(`${req.body.names}`, 16.5, 14);
+
+  doc.setFontSize(14);
+  doc.text(`${req.body.work}`, 16.5, 16.5);
+  doc.setFontSize(11);
+  doc.text(`${req.body.num}`, 9.5, 19);
+  doc.addSvgAsImage("call-outline.svg", 20, 20, 9, 19);
+  doc.save(`ess.pdf`);
+
+  console.log(req.body, "arwarwr");
+  res.json({ jse: "sekjrlwjr" });
 });
 
 app.listen(3000, () => {
